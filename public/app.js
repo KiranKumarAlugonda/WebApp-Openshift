60ba288de3b30f3ee6d814f5150e3a917b05a212
KiranKumarAlugonda <kiran.wdev@gmail.com>
2015-05-13 16:20:48 -0400

    added add function

--- public/app.js
+++ public/app.js
@@ -5,6 +5,13 @@ app.controller("IdeController",
          .success(function (response) {
              $scope.developers = response;
          });
+        $scope.add = function (dev) 
+            {
+                $http.post('/api/developer', dev)
+                .success(function (response) {
+                    $scope.developers = response;
+                })
+            }
 
         $scope.remove = function (index) {
             $http.delete('/api/developer/'+ index)
